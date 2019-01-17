aca40f1c47bead7bb9383e693520e53b2ab729d6
Chris Cirefice <cireficc@mail.gvsu.edu>
2019-01-17 18:58:28 +0100

    Add basic full-text search functionality

--- app/controllers/institutions_controller.rb
+++ app/controllers/institutions_controller.rb
@@ -1,6 +1,8 @@
 class InstitutionsController < ApplicationController
 	
 	def index
-		@institutions = Institution.all.limit(10)
+		keyword = params[:search]
+		
+		@institutions = Institution.search_by_keyword(keyword).reorder(name: :asc).limit(25)
 	end
 end
--- app/models/institution.rb
+++ app/models/institution.rb
@@ -1,2 +1,4 @@
 class Institution < ApplicationRecord
+	include PgSearch
+	pg_search_scope :search_by_keyword, against: [:name, :alias]
 end
